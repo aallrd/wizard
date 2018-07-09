7ff516533269398dd53f17a9157460a0f4d1c6ef
aallrd <antoine.allard@outlook.com>
2018-07-09 13:20:01 +0000

    Serverless deployment using AWS

--- README.md
+++ README.md
@@ -4,17 +4,35 @@ Print a wizard in your shell.
 
 Because you are a **Wizard**.
 
-# Test if the wizard is up
-
-    $ (exec 9>/dev/tcp/wizard.host.fr/1337) 2>/dev/null && { echo "Wizard is up."; } || { echo "Wizard is down."; }
-    $ Wizard is down.
-
-# If the wizard is down, start the wizarding server
-
-    $ python wizarding.py &
-
 # Cast your Wizard spell
 
-    $ echo -e "$({ wget -e use_proxy=no -qO- wizard.host.fr:1337 | openssl enc -base64 -d | gunzip ; } 2>/dev/null )"
+    $ serverless deploy
+    Serverless: Installing requirements of requirements.txt in .serverless...
+    Serverless: Packaging service...
+    Serverless: Excluding development dependencies...
+    Serverless: Injecting required Python packages to package...
+    Serverless: Uploading CloudFormation file to S3...
+    Serverless: Uploading artifacts...
+    Serverless: Uploading service .zip file to S3 (17.37 MB)...
+    Serverless: Validating template...
+    Serverless: Updating Stack...
+    Serverless: Checking Stack update progress...
+    ....................
+    Serverless: Stack update finished...
+    Service Information
+    service: wizard-as-a-service
+    stage: dev
+    region: us-east-1
+    stack: wizard-as-a-service-dev
+    api keys:
+    None
+    endpoints:
+    GET - https://19omnllgei.execute-api.us-east-1.amazonaws.com/dev/
+    GET - https://19omnllgei.execute-api.us-east-1.amazonaws.com/dev/wip
+    functions:
+    magicspell: wizard-as-a-service-dev-magicspell
+    wipspell: wizard-as-a-service-dev-wipspell
+
+    $ curl https://19omnllgei.execute-api.us-east-1.amazonaws.com/dev
 
 > Shazaaamm
--- wizarding.py
+++ wizarding.py
@@ -1,40 +1,56 @@
-#!/usr/bin/env python3
+from colored import fg, bg, attr
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import json
-import base64
-import gzip
-import zlib
-from io import StringIO
-from subprocess import check_output
+def getAsciiWizard():
+    wizard = """\
+                            {black}      {reset}
+                        {black}     {reset}{yellow}    {reset}{black}  {reset}
+                    {black}  {reset}{yellow}      {reset}{orange}  {reset}{black}  {reset}
+                    {black}   {reset}{yellow}      {reset}{orange}  {reset}{black}  {reset}
+                {black}   {reset}{yellow}cccccc{reset}{orange}eeaaaa{reset}{black}  {reset}
+    {black}            {reset}{yellow}cccccc{reset}{orange}eeaaaaaaa{reset}{black}  {reset}
+{black}   {reset}{yellow}ccccccccccccccc{reset}{orange}aaaaaaaaaa{reset}{black}  {reset}
+    {black}   {reset}{yellow}aaaaaaa{reset}{orange}eeceeeeeg{reset}{black}  {reset}
+    {black}      {reset}{yellow}aaaaaaaa{reset}{orange}eeeeeeg{reset}{black}  {reset}
+        {black}        {reset}{yellow}aaaaaa{reset}{orange}eeegg{reset}{black}  {reset}
+            {lgrey}#######{reset}     {reset}{yellow}aaaa{reset}{orange}ggg{reset}{black}  {reset}
+        {lgrey}###{reset}{orange}|¯|{reset}{lgrey}########{reset}{black}  {reset}{yellow}aaaa{reset}{magenta}ggg{reset}{black}  {reset}
+        {black}  {reset}{lgrey}###{reset}{orange}|_|{reset}{lgrey}###{reset}{orange}|¯|{reset}{lgrey}#####{reset}{black}  {reset} {yellow}aa{reset}{magenta}gggg{reset}{black}  {reset}
+    {black}  {reset}{blue}~~{reset}{lgrey}#########{reset}{orange}|_|{reset}{lgrey}########{reset}{black}  {reset}{black}  {reset}{magenta}ggg{reset}{black}  {reset}
+    {black}  {reset}{blue}~~{reset}{black}  {reset}{lgrey}#################{reset}{blue}~~~~~{reset}{black}  {reset}
+    {black}  {reset}{blue}~~~~~~{reset}{lgrey}#########{reset}{blue}~~~~~~~~{reset}{black}  {reset}{blue}~{reset}{black}  {reset}
+    {black}  {reset}{black}  {reset}{blue}~~~~~~~~~~~~{reset}{black}      {reset}{blue}~~~~~{reset}{black}  {reset}
+    {black} {reset}{yellow}$xxx{reset}{black}  {reset}{blue}~~~~~~{reset}{black}  {reset}{black}  {reset}{blue}~~~~~~~~~~~~{reset}{black}  {reset}
+    {black} {reset}{yellow}$xxx{reset}{black}  {reset}{blue}~~~~{reset}{black}      {reset}{blue}~~~~~~~~~~~~{reset}{black}  {reset}
+    {black}    {reset}{blue}~~~~~{reset}{black} {reset}{yellow}$xxx{reset}{black}  {reset}{blue}~~~~~~~~~~~~{reset}{black}  {reset}
+    {black}  {reset}{blue}~~{reset}{black}  {reset}{blue}~~~{reset}{black} {reset}{yellow}$xxx{reset}{black}  {reset}{blue}~~~~~~~~~~~~{reset}{black}  {reset}
+    {black}  {reset}{blue}~~{reset}{black}  {reset}{blue}~~~~{reset}{black}      {reset}{blue}~~~~~~~~~~{reset}{black}  {reset}
+    {black}  {reset}{blue}~~~{reset}{black}  {reset}{blue}~~~~~~~~~{reset}{black}  {reset}{blue}~~~~~~~~{reset}{black}  {reset}
+    {black}  {reset}{blue}~~~~{reset}{black}  {reset} {blue}~~~~~~~{reset}{black}  {reset}{blue}~~~~~~~~{reset}{black}  {reset}
+    {black}  {reset}{blue}~~~~~~~{reset}{black}  {reset}{black}    {reset}{blue}~~~~{reset}{black}  {reset}{blue}~~~~{reset}{black}  {reset}{blue}~~{reset}{black}  {reset}
+{black}  {reset}{blue}~~~~~~~~~~~~~~~~~~~~{reset}{black}  {reset}  {blue}~~~~~~{reset}{black}  {reset}
+    {black}                     {reset}    {black}       {reset}
+{green}~        ARE YOU A WIZARD?        ~{reset}
+    """.format(
+        black=bg('black'),
+        yellow=bg('yellow'),
+        orange=bg('orange_4b'),
+        blue=fg('blue'),
+        green=fg('green'),
+        lgrey=fg('light_gray'),
+        magenta=fg('magenta'),
+        reset=attr('reset'))
+    return wizard
 
-HOSTNAME = "0.0.0.0"
-PORT = 1337
+def getTerminalWizard():
+    wizard = None
+    with open('timg_wizard.txt', 'r') as f:
+        wizard = f.read()
+    return wizard
 
-def getEncodedWizardSystem():
-    base64_wizard = None
-    with open('wizard.ascii', 'r', encoding='utf-8') as f:
-        wizard = bytes(f.read(), 'utf-8')
-        gzipped_wizard = gzip.compress(wizard)
-        base64_wizard = base64.b64encode(gzipped_wizard)
-    return base64_wizard
+def wipspell(event, context):
+    response = getAsciiWizard()
+    return response
 
-# Should be the same as cat wizard.ascii | gzip | openssl enc -base64
-# Working on Solaris, not on Linux somehow...
-def getEncodedWizard():
-    encoded = StringIO()
-    with open('wizard.ascii', 'r') as wizard:
-        with gzip.GzipFile(fileobj=encoded, mode="w") as gzipped:
-            gzipped.write(wizard.read())
-    return base64.b64encode(encoded.getvalue())
-
-class MyHandler(BaseHTTPRequestHandler):
-    def do_GET(s):
-        s.send_response(200)
-        s.send_header("Content-type", "text/plain")
-        s.end_headers()
-        s.wfile.write(getEncodedWizardSystem())
-
-if __name__ == '__main__':
-    http_server = HTTPServer((HOSTNAME, PORT), MyHandler)
-    http_server.serve_forever()
+def spell(event, context):
+    response = getTerminalWizard()
+    return response
